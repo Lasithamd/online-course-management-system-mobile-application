117a7190271d3b7db19374affe6f93e99636c2e2
Lasitha_madhuranga <lasithadayarathna@gmail.com>
2024-10-04 20:34:59 +0530

    paper install

---
+++ App.js
@@ -0,0 +1,58 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import type {PropsWithChildren} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View
+} from 'react-native';
+import { Button } from 'react-native-paper';
+
+
+function App() {
+  const onPressLearnMore= () =>{
+    console.log('sdsdds');
+    
+
+  }
+
+  return (
+    <SafeAreaView >
+      <Text>Hii</Text>
+      <Button icon="camera" mode="contained" onPress={() => console.log('Pressed')}>
+    Press me
+  </Button>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: '600',
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+});
+
+export default App;
