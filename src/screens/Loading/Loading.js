e11277d59298da74ebe4306aab9ca065077d3d8c
Lasitha_madhuranga <lasithadayarathna@gmail.com>
2024-10-11 13:58:28 +0530

    Login Page UI Redesign

--- src/screens/Loading/Loading.js
+++ src/screens/Loading/Loading.js
@@ -1,19 +1,20 @@
 // LoadingScreen.js
 import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import { ActivityIndicator, Text } from 'react-native-paper';
+import { ActivityIndicator,MD2Colors , Text } from 'react-native-paper';
 
 const Loading = () => {
   return (
     <View>
-    <ActivityIndicator animating={true} />
+    <ActivityIndicator animating={true} color={MD2Colors.blue800} size={50} style={styles.middle} />
+    <Text style={styles.loadingText}>Loading...</Text>
   </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
+  
     justifyContent: 'center',
     alignItems: 'center',
     backgroundColor: '#fff', // You can change this to your preferred background color
@@ -21,7 +22,13 @@ const styles = StyleSheet.create({
   loadingText: {
     marginTop: 20,
     fontSize: 16,
+    textAlign:"center"
   },
+  middle:{
+    marginTop:"80%",
+    
+  },
+
 });
 
 export default Loading;
--- src/screens/Login/Login.js
+++ src/screens/Login/Login.js
@@ -1,87 +1,103 @@
-import React,{useState} from 'react';
+import React, { useState } from 'react';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
-
+import { TextInput, Button } from 'react-native-paper';
 import {
-    View, TextInput,
-    KeyboardAvoidingView,
-    StyleSheet,
-    Text,
-    Platform,
-    TouchableWithoutFeedback,
-    Button,
-    Keyboard,
-  } from 'react-native';
+  View,
+  KeyboardAvoidingView,
+  StyleSheet,
+  Text,
+  Platform,
+  TouchableWithoutFeedback,
+
+  Keyboard, Image
+} from 'react-native';
 function Login({ navigation }) {
 
-  const [credential, setCredential]= useState('');
-    function onLogin() {
-   
+  const [credential, setCredential] = useState('');
+  //     function onLogin() {
 
-    const data = {
-      email: 'chamikara12345@gmail.com',
-      password: '12345'
-  };
-  
-  axios.post('http://student-api.acpt.lk/api/login', data)
-      .then(response => {
-        const token = response.data.token;
 
-        // Store the token in AsyncStorage
-        AsyncStorage.setItem('userToken', token);
-       
-        navigation.navigate('Home');  
-          console.log(response.data);
-      })
-      .catch(err => {
-          console.error(err);
-      });
-  
-}
+  //     const data = {
+  //       email: 'chamikara12345@gmail.com',
+  //       password: '12345'
+  //   };
 
-    const [text, setText] = React.useState("");
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-    return (
-        <KeyboardAvoidingView
+  //   axios.post('http://student-api.acpt.lk/api/login', data)
+  //       .then(response => {
+  //         const token = response.data.token;
+
+  //         // Store the token in AsyncStorage
+  //         AsyncStorage.setItem('userToken', token);
+
+  //         navigation.navigate('Home');  
+  //           console.log(response.data);
+  //       })
+  //       .catch(err => {
+  //           console.error(err);
+  //       });
+
+  // }
+
+  const [text, setText] = React.useState("");
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  return (
+    <KeyboardAvoidingView
       behavior={Platform.OS === 'ios' ? 'padding' : 'height'}
       style={styles.container}>
       <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={styles.inner}>
-          <Text style={styles.header}>Header</Text>
-          <TextInput placeholder="Username"  onChangeText={text => setEmail(text)} style={styles.textInput} />
-          <TextInput placeholder="Password" onChangeText={text => setPassword(text)} style={styles.textInput} />
+          <Text style={styles.logoText}>C M S</Text>
+
+          <Text style={styles.slogon}>Course Management System</Text>
+          <Text style={styles.header}>Student Login</Text>
+          <TextInput label="Email" onChangeText={text => setEmail(text)} style={styles.textInput} mode="outlined" />
+          <TextInput label="Password" onChangeText={text => setPassword(text)} style={styles.textInput} mode="outlined" />
           <View style={styles.btnContainer}>
-            <Button title="Submit" onPress={() => onLogin()} />
+            <Button style={styles.btn} mode="contained" onPress={() => console.log('Pressed')} buttonColor={'#f39c12'}>
+              Login
+            </Button>
+            <Button onPress={() => onLogin()} />
           </View>
         </View>
       </TouchableWithoutFeedback>
     </KeyboardAvoidingView>
 
-    );
+  );
 }
 const styles = StyleSheet.create({
-    container: {
-   
-    },
-    inner: {
-      padding: 24,
-    
-      justifyContent: 'space-around',
-    },
-    header: {
-      fontSize: 36,
-      marginBottom: 48,
-    },
-    textInput: {
-      height: 40,
-      borderColor: '#000000',
-      borderBottomWidth: 1,
-      marginBottom: 36,
-    },
-    btnContainer: {
-      backgroundColor: 'white',
-      marginTop: 12,
-    },
-  });
+  container: {
+    height: '100%',
+    backgroundColor: "#00ade9", justifyContent: 'center'
+  },
+  image: {
+    width: 50, height: 50,
+    textAlign: 'center',
+
+  },
+  inner: {
+    padding: 24,
+
+    justifyContent: 'space-around',
+  },
+  logoText: {
+    fontSize: 42,
+    textAlign: "center",
+    fontWeight: '800',
+    color: 'white'
+  },
+  slogon: {
+    color: 'white',
+    textAlign: "center",
+  },
+  header: {
+    color: 'white',
+    textAlign: "center", fontSize: 24,
+    marginTop: 90
+  },
+  btn: {
+    marginTop: 5
+  }
+});
 export default Login;
